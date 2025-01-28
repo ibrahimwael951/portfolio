cb702d7706dffb9b9c66ba9b49c9ca8071447164
ibrahim wael <ibrahimwael809@gmail.com>
2025-01-28 16:08:57 +0200

    Add files via upload

--- src/componants/works/works.js
+++ src/componants/works/works.js
@@ -32,6 +32,8 @@ const [height, setheight] = useState(0);
       LinkPage: "https://flix.kareemadel.com/en",
       LinkGithub: "https://github.com/KareemAdelAwwad/flix",
       responsive: "yes",
+      youTubeLink: "none",
+      underdevelopment: "NO",
     },
     {
       src: project2,
@@ -40,22 +42,31 @@ const [height, setheight] = useState(0);
       LinkGithub: "https://github.com/ibrahimwael951/school-repo",
       LinkPage: "https://appschool.vercel.app/",
       responsive: "yes",
+      youTubeLink: "none",
+      underdevelopment: "yes",
+
     },
     {
       src: project3,
       title: 'Car modification company',
       description: 'A site about the car modification company and the site explains its services and modifications available and how to communicate with them. The site is designed and developed using HTML, CSS, and JavaScript.',
-      LinkPage: "https://ibrahimwael951.github.io/Car-Company/",
+      LinkPage: "none",
       LinkGithub: "https://github.com/ibrahimwael951/Car-Company",
       responsive: "yes",
+      youTubeLink: "https://youtu.be/lKjGEWjFxvA",
+      underdevelopment: "NO",
+
     },
     {
       src: project4,
       title: 'Shoes Shop',
       description: 'Designing a website for a shoe shop to display company details and allow users to log in and purchase. Designed using HTML, CSS, and JavaScript, and is still under development.',
-      LinkPage: "https://ibrahimwael951.github.io/shoes-shop/",
+      LinkPage: "none",
       LinkGithub: "https://github.com/ibrahimwael951/shoes-shop/",
       responsive: "NO",
+      youTubeLink: "https://youtu.be/WZPxhautt9g",
+      underdevelopment: "NO",
+
     },
     {
       src: project5,
@@ -64,15 +75,21 @@ const [height, setheight] = useState(0);
       LinkPage: "https://ibrahimwael951.github.io/calc-js/",
       LinkGithub: "https://github.com/ibrahimwael951/calc-js",
       responsive: "yes",
+      youTubeLink: "https://youtu.be/UmB1GldJ6pI",
+      underdevelopment: "NO",
+
     },
-    {
-      src: project6,
-      title: 'Content Creator Portfolio',
-      description: 'A site that displays the content creator portfolio, designed using HTML, CSS, and JavaScript, and is still under development.',
-      LinkPage: "none",
-      LinkGithub: "none",
-      responsive: "No",
-    }
+    // {
+    //   src: project6,
+    //   title: 'Content Creator Portfolio',
+    //   description: 'A site that displays the content creator portfolio, designed using HTML, CSS, and JavaScript, and is still under development.',
+    //   LinkPage: "none",
+    //   LinkGithub: "none",
+    //   responsive: "No",
+    //   youTubeLink: "none",
+    //   underdevelopment: "yes",
+
+    // }
   ];
 
   const handleImageClick = (image) => {
@@ -87,6 +104,8 @@ const [height, setheight] = useState(0);
       LinkGithub: image.LinkGithub,
       LinkPage: image.LinkPage,
       Responsive: image.responsive,
+      youTubeLink: image.youTubeLink,
+      underdevelopment: image.underdevelopment
     });
    
     
@@ -122,28 +141,46 @@ const [height, setheight] = useState(0);
                                  <div className='detailspage-right'>
                                     <h2>{details.title}</h2>
                        
-                                    {details.LinkGithub === "none" ? (
+
+
+
+            {/* Under development state  */}
+            {details.underdevelopment === "NO" ?  "" : <p style={{backgroundColor:"red"}}>Under Development</p>}
+
+
+                        {/* Github Link */}
+            {details.LinkGithub === "none" ? (
                 <p style={{backgroundColor:"red"}}>no Link Github : Private</p>
               ) : (
                 <a href={details.LinkGithub}>Link Github : Click Me</a>
               )}
 
+
+                    {/* Page Link */}
                   {details.LinkPage === "none" ? (
-                <p style={{backgroundColor:"red"}}>no Link Github</p>
+                 ""
               ) : (
                 <a href={details.LinkPage}>Link Page : Click Me</a>
               )} 
 
-                                    {/* <p>responsive : {details.Responsive} </p> */}
+                          {/* youtube Links*/}   
+              { details.youTubeLink === "none" ?
+              (""):
+                    (
+                        <a href={details.youTubeLink}>youTube Link : Click Me</a>
+                    )
+              }
+              
+                    {/* Responsive Web */}
+                 {details.Responsive === "yes" ? (                
+                      <p style={{backgroundColor:"green"}}>Responsive : Yes</p>
+                    ):
+                      (
+                       <p style={{backgroundColor:"red"}}>Not Responsive</p>
+                      )
+                }
 
-                                    {details.Responsive === "yes" ? (
-                                     
-                                      <p style={{backgroundColor:"green"}}>Responsive : Yes</p>):
-                                      (
-                                      <p style={{backgroundColor:"red"}}>Not Responsive</p>
-                                    )
-                                      }
-                                   <button onClick={handleCloseDetails }>x</button>
+           <button onClick={handleCloseDetails }>x</button>
                                    
                             </div>
                     </div>
